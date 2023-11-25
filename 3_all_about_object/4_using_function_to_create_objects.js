f2742917448c3997153b94c4501bc16ad02e4619
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2023-11-25 16:05:18 +0900

    Update 4_using_function_to_create_objects.js

--- 3_all_about_object/4_using_function_to_create_objects.js
+++ 3_all_about_object/4_using_function_to_create_objects.js
@@ -14,7 +14,7 @@ function IdolModel(name, year){
     }
 }
 
-const yuJin = new IdolModel('안유진', 2003);
+const yuJin = new IdolModel('안유진', 2003); // new 키워드는 생성자 함수를 어디에다 맵핑할 지를 결정??
 console.log(yuJin);
 // console.log(yuJin.dance());
 const yuJin2 = IdolModel('안유진', 2003); // 개발환경에 따라 생성자 함수에 new 키워드 미사용시 에러 발생으로 다음 코드 실행 불가
