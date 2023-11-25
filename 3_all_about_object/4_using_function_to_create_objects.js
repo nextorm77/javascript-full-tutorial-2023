439d77ae6ca1eb2529549b813c629b095620852e
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2023-11-25 15:58:39 +0900

    Update 4_using_function_to_create_objects.js

--- 3_all_about_object/4_using_function_to_create_objects.js
+++ 3_all_about_object/4_using_function_to_create_objects.js
@@ -17,8 +17,8 @@ function IdolModel(name, year){
 const yuJin = new IdolModel('안유진', 2003);
 console.log(yuJin);
 // console.log(yuJin.dance());
-const yuJin2 = IdolModel('안유진', 2003); 
-console.log(yuJin2); // new 키워드 미사용시 'undefined' 출력, 반환값이 없기 때문
+const yuJin2 = IdolModel('안유진', 2003); // 개발환경에 따라 생성자 함수에 new 키워드 미사용시 에러 발생으로 다음 코드 실행 불가
+console.log(yuJin2); 
 // console.log(global.name);
 
 const IdolModelArrow = (name, year)=>{
