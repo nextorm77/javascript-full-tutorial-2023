e0cf955457c6a40994c9306c01b859441aa40527
nextorm77 <90032559+nextorm77@users.noreply.github.com>
2024-02-13 16:36:51 +0900

    Update 3_async_await.js

--- 4_async_programming/3_async_await.js
+++ 4_async_programming/3_async_await.js
@@ -9,7 +9,7 @@ const getPromise = (seconds)=> new Promise((resolve, reject) => {
 
 // 콜백 지옥의 최종 해결 방안?
 async function runner(){
-    // try내의 비동기 코드(getPromise내)가 순서대로 실행 => result1 인쇄 -> result2 인쇄 -> result3 인쇄
+    // try내의 비동기 코드(getPromise내)가 순서대로 실행 => (1초후) result1 인쇄 -> (2초후) result2 인쇄 -> (1초후) result3 인쇄 => 총 4초 소요!!
     try{
         const result1 = await getPromise(1); // 성공(resolve() 실행)의 경우 resolve 인수 리턴, 실패(reject() 실행) 의 경우 catch 블록 이동?
         console.log(result1);
